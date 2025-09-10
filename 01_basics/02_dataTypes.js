3cf1da06132ea7e6934c2514ee43b1fc3a8146ab
isamzia11 <isamzia091@gmail.com>
2025-09-10 17:07:32 +0500

    created 02_dataTypes file

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,24 @@
+"use strict";  // treat all js code as newer version 
+//alert(3+3) // we are using nodeJs not browser 
+
+
+let name = "Isam" 
+let age  = 18
+let isLoggedIn = true 
+
+console.log(typeof name)
+console.log(typeof null) // type = object
+console.log(typeof undefined) // type = undefined
+/*
+PRIMITIVE DATA-TYPES
+number => 2 to pwer 53
+bigint 
+String => "" 
+boolean => true/false
+null => standalone value 
+undefined => 
+symbol => unique    
+
+object
+
+*/
