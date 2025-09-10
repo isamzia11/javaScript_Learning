804197d6436bd4fb1654a56b309d9658a0ab2dfd
isamzia11 <isamzia091@gmail.com>
2025-09-10 15:32:15 +0500

    created 01_variables.js

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId = 144553
+let accountEmail = 'isam@gmail.com' 
+var accountPassword = "12345"
+accountCity = "Peshawar" 
+let accountState;
+
+
+/*
+Prefer not use var 
+because of issue in block scope and functional scope 
+*/
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
