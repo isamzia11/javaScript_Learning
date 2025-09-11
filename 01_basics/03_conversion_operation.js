23cbe75a6f07fcc1429395a9d4a87524c2b284db
isamzia11 <isamzia091@gmail.com>
2025-09-11 13:09:04 +0500

    performed some conversion operations

--- 01_basics/03_conversion_operation.js
+++ 01_basics/03_conversion_operation.js
@@ -4,3 +4,29 @@ console.log(typeof (number))
 let valueInNumber = Number(number) 
 console.log(typeof valueInNumber) 
 console.log(valueInNumber)
+
+/*
+"33" => 33 easily converted 
+"33abc" => NaN cannot be converted 
+true => 1 , false => 0
+
+*/
+
+// **************** operations ****************
+
+let value = 3 
+let negValue = -value
+console.log(negValue)
+console.log("1" + 2) 
+console.log(1+ "2") 
+console.log("1" + 2 + 2) 
+console.log(1 + 2 + "2") 
+
+let x = 3
+let y = x++ 
+console.log(`x:${x}, y:${y}`) 
+
+
+let a = 3
+let b  = ++a 
+console.log(`a:${a}, b:${b}`)
